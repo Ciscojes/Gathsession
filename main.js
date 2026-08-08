fb8ccdd810596aceed9c0057f8ccb8b2d7a486e2
Jesus Granados <jfrangranadosmora@gmail.com>
2026-08-07 22:08:42 -0600

    Build hero section and local typography

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+import "@fontsource/merriweather/latin-700.css";
+import "@fontsource/poppins/latin-400.css";
+import "@fontsource/poppins/latin-600.css";
 import "./sass/style.scss";
 
 const yearElement = document.querySelector("#current-year");
